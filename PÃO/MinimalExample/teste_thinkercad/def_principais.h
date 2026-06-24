4556a87581f5337efd0894b82689f605d9167f31
LuisHenrique <lhfspro@gmail.com>
2026-06-24 17:00:56 -0300

    feat:panificadora inicial pronta

---
+++ PÃO/MinimalExample/teste_thinkercad/def_principais.h
@@ -0,0 +1,17 @@
+#ifndef _DEF_PRINCIPAIS_H
+#define _DEF_PRINCIPAIS_H
+
+#define F_CPU 16000000UL	//define a frequencia do microcontrolador - 16MHz
+
+#include <avr/io.h> 	    //defini��es do componente especificado
+#include <util/delay.h>		//biblioteca para o uso das rotinas de _delay_ms e _delay_us()
+#include <avr/pgmspace.h>   //para o uso do PROGMEM, grava��o de dados na mem�ria flash
+
+
+#define	set_bit(y,bit)	(y|=(1<<bit))	//coloca em 1 o bit x da vari�vel Y
+#define	clr_bit(y,bit)	(y&=~(1<<bit))	//coloca em 0 o bit x da vari�vel Y
+#define cpl_bit(y,bit) 	(y^=(1<<bit))	//troca o estado l�gico do bit x da vari�vel Y
+#define tst_bit(y,bit) 	(y&(1<<bit))	//retorna 0 ou 1 conforme leitura do bit
+
+#endif
+
